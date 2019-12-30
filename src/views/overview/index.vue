6044326dc53ebac7aeda58535f6575f373efbf32
wind34012614 <wind@wind123.com>
2019-12-31 00:50:27 +0800

    map
    from wind

--- src/views/overview/index.vue
+++ src/views/overview/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="yqbg">
-  <img src="@images/bgright.png" style="position:absolute;right:0;top:0px;width:18%;">
+  <img src="@images/bgright.png" style="position:absolute;right:0;top:20px;width:18%;">
   <div style="width:100%;clear:both;margin:0 auto;"></div>
   </div>
 </template>
@@ -17,7 +17,9 @@ export default {
   background-image:url("~@images/map.png");
   height:100%;
   width:100%;
-  background-size: 126%;
-  background-position: -260px -271px;
+  background-size: 110%;
+  background-position: -150px -100px;
+  background-repeat: no-repeat;
+  background-color:#171717;
 }
 </style>
